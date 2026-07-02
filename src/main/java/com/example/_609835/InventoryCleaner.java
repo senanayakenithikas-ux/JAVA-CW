0a6cbf2d3bb11e05bd32950c6e24ec16ae1fb7dc
Nithika_zen <senanayakenithikas@gmail.com>
2026-07-03 00:08:19 +0530

    added null-safe checker

--- src/main/java/com/example/_609835/InventoryCleaner.java
+++ src/main/java/com/example/_609835/InventoryCleaner.java
@@ -1,4 +1,9 @@
 package com.example._609835;
 
 public class InventoryCleaner {
+    public static String[] splitLine(String line){
+        if (line == null || line.trim().isEmpty()){
+            return new String[0];
+        }
+    }
 }
