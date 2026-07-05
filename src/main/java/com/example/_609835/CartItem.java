a99d3b61a3c350e9d63dcf31abea0cecceb1e1f0
Nithika_zen <senanayakenithikas@gmail.com>
2026-07-05 19:50:05 +0530

    Add CartItem model class with fields, constructor, getters and setters

---
+++ src/main/java/com/example/_609835/CartItem.java
@@ -0,0 +1,28 @@
+package com.example._609835;
+
+public class CartItem {
+    private Part part;
+    private int quantity;
+
+    public CartItem(Part part, int quantity){
+        this.part = part;
+        this.quantity = quantity;
+    }
+    public Part getPart() {
+        return part;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setPart(Part part) {
+        this.part = part;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+
+}
