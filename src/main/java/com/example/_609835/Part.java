8eb570f71e7f682e02c482e3b3b79a771ace37c8
Nithika_zen <senanayakenithikas@gmail.com>
2026-07-05 18:55:40 +0530

    Add Part model class with fields, constructor, getters and setters

---
+++ src/main/java/com/example/_609835/Part.java
@@ -0,0 +1,88 @@
+package com.example._609835;
+
+public class Part {
+    private String partCode;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPartCode() {
+        return partCode;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public String getDataAdded() {
+        return dateAdded;
+    }
+
+    public String getImageFile() {
+        return imageFile;
+    }
+
+    public void setPartCode(String partCode) {
+        this.partCode = partCode;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public void setImageFile(String imageFile) {
+        this.imageFile = imageFile;
+    }
+
+    public void setDataAdded(String dataAdded) {
+        this.dateAdded = dataAdded;
+    }
+
+    private String name;
+    private String brand;
+    private double price;
+    private int quantity;
+    private String category;
+    private String dateAdded;
+    private String imageFile;
+
+    public Part(String partCode, String name,  String brand, double price, int quantity, String category, String dataAdded, String imageFile){
+        this.partCode = partCode;
+        this.name = name;
+        this.brand = brand;
+        this.price = price;
+        this.quantity = quantity;
+        this.category = category;
+        this.dateAdded = dataAdded;
+        this.imageFile = imageFile;
+    }
+}
