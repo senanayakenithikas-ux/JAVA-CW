e8d5b3a599adb10ae6c8c69aed5ea65a41f7ad5e
Nithika_zen <senanayakenithikas@gmail.com>
2026-07-05 19:30:38 +0530

    Add Dealer model class with fields, constructor, getters and setters

---
+++ src/main/java/com/example/_609835/Dealer.java
@@ -0,0 +1,51 @@
+package com.example._609835;
+
+public class Dealer {
+    private String dealerCode;
+    private String name;
+    private String phone;
+    private String location;
+
+    public Dealer(String dealerCode, String name, String phone, String location){
+        this.dealerCode = dealerCode;
+        this.name = name;
+        this.phone = phone;
+        this.location = location;
+    }
+
+    public String getDealerCode() {
+        return dealerCode;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setDealerCode(String dealerCode) {
+        this.dealerCode = dealerCode;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+
+
+
+}
